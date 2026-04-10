c25af81914201dec64bac21d35f84dddcf66d5f0
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-04-10 10:52:22 -0300

    feat - adicionando controller

---
+++ imageliteapi/src/main/java/github/com/MaxBr221/imageliteapi/controller/ImagesController.java
@@ -0,0 +1,30 @@
+package github.com.MaxBr221.imageliteapi.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/images")
+@Slf4j
+public class ImagesController {
+
+
+    @PostMapping
+    public ResponseEntity saveImage(@RequestParam("file") MultipartFile file,
+                                    @RequestParam("name") String name,
+                                    @RequestParam("tags") List<String> tags
+    ){
+        log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
+        log.info("Nome da imagem: imagemName: {}", name);
+        log.info("Tags: {},", tags);
+        return ResponseEntity.ok().build();
+    }
+}
